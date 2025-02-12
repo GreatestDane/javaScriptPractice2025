65a895b843af9633686e746bf177c45967a65f75
Dane Goodman <danegoodman85@gmail.com>
2025-02-12 15:50:01 -0800

    last commit before I start Cypress

--- javascript/class.js
+++ javascript/class.js
@@ -36,3 +36,60 @@ class Person {
 hank = new Person("Hank", 39, "male", "music"); 
 
 hank.speak(); 
+
+
+//Extending a class can borrow the attributes of another existing class, like so: 
+class Animal {
+    constructor(name) {
+        this.name = name;
+    }
+
+    speak() {
+        console.log(`${this.name} makes a noise.`);
+    }
+}
+
+// Dog class extends Animal and can now use it's attributes 
+class Dog extends Animal {
+    speak() {
+        console.log(`${this.name} barks!`);
+    }
+}
+
+let dog = new Dog("Buddy");
+dog.speak(); // Buddy barks!
+
+
+//Okay, let's try another SUPER practice
+
+
+class Hero {
+    constructor(name, affiliation, power) {
+        this.name = name,
+        this.affiliation = affiliation,
+        this.power = power
+    }
+    usePower() {
+        console.log(`${this.name} the ${this.affiliation}, uses his ${this.power} power in a shower of force!`);
+    }
+};
+
+superMan = new Hero("superMan", "hero", "laser eyes")
+
+superMan.usePower();
+
+class Antihero extends Hero {
+    constructor(name, affiliation, power, guns) {
+        super(name, affiliation, power)
+        this.guns = guns
+    }
+    usePower() {
+        super.usePower()
+        console.log (`And then he takes out his ${this.guns} and blasts a hole through the bad guys`);
+    }
+
+};
+
+let superPunisher = new Antihero("SuperPunisher", "AntiHero", "grenade bombs", "twin glocks");
+
+superPunisher.usePower();
